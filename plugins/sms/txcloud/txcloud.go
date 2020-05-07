33915ab76489c712a998be2f804a1b92295001af
vvvvvgod <645976627@qq.com>
2020-05-07 12:53:32 +0800

    sms plugin

---
+++ plugins/sms/txcloud/txcloud.go
@@ -0,0 +1,34 @@
+package txcloud
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20190711"
+)
+
+var client *sms.Client
+
+func init() {
+	/* 必要步骤：
+	 * 实例化一个认证对象，入参需要传入腾讯云账户密钥对secretId，secretKey。
+	 * 这里采用的是从环境变量读取的方式，需要在环境变量中先设置这两个值。
+	 * 你也可以直接在代码中写死密钥对，但是小心不要将代码复制、上传或者分享给他人，
+	 * 以免泄露密钥对危及你的财产安全。
+	 * CAM密匙查询: https://console.cloud.tencent.com/cam/capi*/
+	credential := common.NewCredential(
+		// os.Getenv("TENCENTCLOUD_SECRET_ID"),
+		// os.Getenv("TENCENTCLOUD_SECRET_KEY"),
+		"xxx",
+		"xxx",
+	)
+	var err error
+	client, err = sms.NewClient(credential, "ap-guangzhou")
+	if err != nil {
+		// Handle err
+	}
+}
+
